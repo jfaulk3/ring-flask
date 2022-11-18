15bc12f9dc4bd4483f451d8c9ab56e1d8450ae74
jfaulk3 <admin@gretrix.com>
2022-11-18 15:22:08 -0600

    Added git ignore, cleaned up code

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from flask_restful import Api, Resource, abort, request
 from ring_doorbell import Ring, Auth
-import json
-from pathlib import Path
 from oauthlib.oauth2 import MissingTokenError
 from flask_cors import CORS
 
@@ -10,39 +8,36 @@ app = Flask (__name__)
 api = Api(app)
 CORS(app)
 
-cache_file = Path("test_token.cache")
-
-def token_updated(token):
-    cache_file.write_text(json.dumps(token))
 
 class Code(Resource):
   def post(self):
-    username = request.forms.get('username')
-    password = request.forms.get('password')
-
-    auth = Auth("PorchPals/1.0", None, token_updated)
+    username = request.form.get('username')
+    password = request.form.get('password')
+    auth = Auth("PorchPals/1.0", None)
     try:
         auth.fetch_token(username, password)
     except MissingTokenError:
         return {"data": "Code has been sent"}, 201
-    except:
-        abort(401, message="Username or Password is not valid...")
+    except Exception as e:
+       return {"data": f'Username, or Password is not valid...Exception is: {e}'}, 401
     return {"data": "Code has been sent"}, 201
+
+  def get(self):
+    return "Code Page"
 
 class Video(Resource):
   def post(self):
-    username = request.forms.get('username')
-    password = request.forms.get('password')
-    code = request.forms.get('code') or None
+    username = request.form.get('username')
+    password = request.form.get('password')
+    code = request.form.get('code') or None
 
-    auth = Auth("PorchPals/1.0", None, token_updated)
+    auth = Auth("PorchPals/1.0", None)
     try:
-        auth.fetch_token(username, password)
+      auth.fetch_token(username, password)
     except MissingTokenError:
-        auth.fetch_token(username, password, code)
-    except:
-      abort(401, message="Invalid Credentials...")
-
+      auth.fetch_token(username, password, code)
+    except Exception as e:
+      return {"data": f'Username, or Password is not valid...Exception is: {e}'}, 401
     ring = Ring(auth)
     ring.update_data()
     devices = ring.devices()
