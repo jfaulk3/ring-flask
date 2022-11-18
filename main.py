107f9921072829c3eaa85fb81719b6d0cb427dd1
jfaulk3 <admin@gretrix.com>
2022-11-18 09:43:54 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from flask import Flask
 from flask_restful import Api, Resource, abort, request
 from ring_doorbell import Ring, Auth
-"""Python Ring Doorbell wrapper."""
 import json
 from pathlib import Path
 from oauthlib.oauth2 import MissingTokenError
+from flask_cors import CORS
 
 app = Flask (__name__)
 api = Api(app)
+CORS(app)
 
-class getCode(Resource):
+cache_file = Path("test_token.cache")
+
+def token_updated(token):
+    cache_file.write_text(json.dumps(token))
+
+class Code(Resource):
   def post(self):
     username = request.forms.get('username')
     password = request.forms.get('password')
@@ -18,16 +24,39 @@ class getCode(Resource):
     try:
         auth.fetch_token(username, password)
     except MissingTokenError:
-        return {"data": ""}, 201
+        return {"data": "Code has been sent"}, 201
     except:
         abort(401, message="Username or Password is not valid...")
-    return {"data": ""}, 201
+    return {"data": "Code has been sent"}, 201
+
+class Video(Resource):
+  def post(self):
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+    code = request.forms.get('code') or None
+
+    auth = Auth("PorchPals/1.0", None, token_updated)
+    try:
+        auth.fetch_token(username, password)
+    except MissingTokenError:
+        auth.fetch_token(username, password, code)
+    except:
+      abort(401, message="Invalid Credentials...")
+
+    ring = Ring(auth)
+    ring.update_data()
+    devices = ring.devices()
+    doorbell = devices['doorbots'][0]
+    videoUrl = doorbell.recording_url(doorbell.last_recording_id)
+    return {"data": videoUrl}, 200
 
 class HelloWorld(Resource):
   def get(self):
-    return {"data": "Hello World!"}
+    return {"data": "Hello World!"}, 200
 
-api.add_resource(getCode,"/getcode")
-api.add_resource(HelloWorld, "/helloworld")
+api.add_resource(Code,"/code")
+api.add_resource(Video,"/video")
+api.add_resource(HelloWorld, "/")
+
 if __name__ == "__main__":
   app.run(debug=True)
